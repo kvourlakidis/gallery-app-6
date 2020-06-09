08c4b28aa8a4fdce2d75e2279d816f6f103d0aa9
Kyrilos Vourlakidis <kvourlakidis@gmail.com>
2020-06-09 12:55:13 +0100

    Add index action to welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,10 @@
 class WelcomeController < ApplicationController
+def index
+    @artworks = active_artworks
+end
+
+private
+    def active_artworks
+        Artwork.active
+    end
 end
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,6 +1,10 @@
 class Artwork < ApplicationRecord
     has_one_attached :cover_image
     has_many :artwork_parts, dependent: :destroy
-    validates :title, presence: true, length: { minimum: 5 }
+    validates :title, 
+        presence: true, 
+        length: { minimum: 5 },
+        uniqueness: { message: 'already exists!' }
     validates :cover_image, presence: true
+    scope :active, -> { where(active: true) }
 end
